9514b2c70aa56533f8b9f3b89224a0986329202c
Rakeshgowda <130374347+rakeshgowda123@users.noreply.github.com>
2025-03-06 15:56:18 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL ='roxiler-assignment-rakesh-gowda.vercel.app';
+const API_BASE_URL ='https://roxilerassignment-rakeshgowda.onrender.com';
 
 
 export interface Transaction {
